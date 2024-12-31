6f5612ce771d96be10b0045ed8bce784bda26a26
Eduardo Tavares <edut.santosdev@gmail.com>
2024-12-31 17:05:27 -0300

    mapeando propriedades

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,9 +1,16 @@
 import { Entity } from "../../core/entities/entity"
+import type { UniqueEntityID } from "../../core/entities/unique-entity-id"
+import type { Slug } from "./value-objects/slug"
 
 interface QuestionProps {
+  authorId: UniqueEntityID
+  questionId: UniqueEntityID
+  bestAnswerId?: UniqueEntityID
   title: string
   content: string
-  authorId: string
+  slug: Slug
+  createdAt: Date
+  updatedAt?: Date
 }
 
 export class Question extends Entity<QuestionProps> {}
