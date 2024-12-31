c2f3243e52b00f5f03efe25144040f2bc2eb82ce
Eduardo Tavares <edut.santosdev@gmail.com>
2024-12-31 17:16:07 -0300

    abstraindo criação de entidades

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,5 +1,6 @@
 import { Entity } from "../../core/entities/entity"
 import type { UniqueEntityID } from "../../core/entities/unique-entity-id"
+import type { Optional } from "../../core/types/optional"
 
 interface AnswerProps {
   authorId: UniqueEntityID
@@ -13,4 +14,16 @@ export class Answer extends Entity<AnswerProps> {
   get content() {
     return this.props.content
   }
+
+    static create(props: Optional<AnswerProps, 'createdAt'>, id?: UniqueEntityID) {
+      const answer = new Answer(
+        {
+          ...props,
+          createdAt: props.createdAt ?? new Date()
+        },
+        id
+      )
+  
+      return answer
+    }
 }
