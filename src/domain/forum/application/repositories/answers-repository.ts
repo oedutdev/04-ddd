5db3722e3dc14178c70f40edd4eea7c60fdc7c8f
Eduardo Tavares <edut.santosdev@gmail.com>
2025-01-02 01:27:32 -0300

    caso de uso: buscar pergunta pelo slug

--- src/domain/forum/application/repositories/answers-repository.ts
+++ src/domain/forum/application/repositories/answers-repository.ts
@@ -1,4 +1,4 @@
-import { Answer } from '@/domain/enterprise/entities/answer'
+import { Answer } from '@/domain/forum/enterprise/entities/answer'
 
 export interface AnswersRepository {
   create(answer: Answer): Promise<void>
