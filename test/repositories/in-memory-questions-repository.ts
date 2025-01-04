7bcc72de5c23db457329915b2c24f39e1dcd89cd
Eduardo Tavares <edut.santosdev@gmail.com>
2025-01-03 23:20:04 -0300

    caso de uso: listar perguntas recentes

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,3 +1,4 @@
+import type { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
 import { Question } from '@/domain/forum/enterprise/entities/question'
 
@@ -16,10 +17,18 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
 
   async findBySlug(slug: string) {
     const question = this.items.find((item) => item.slug.value === slug)
+
     if (!question) {
       return null
     }
     return question
+  }
+
+  async findManyRecent({ page }: PaginationParams) {
+    const questions = this.items
+      .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+      .slice((page - 1) * 20, page * 20)
+    return questions
   }
 
   async create(question: Question) {
@@ -28,6 +37,7 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
 
   async save(question: Question) {
     const itemIndex = this.items.findIndex((item) => item.id === question.id)
+
     this.items[itemIndex] = question
   }
 
